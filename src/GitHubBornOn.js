920028354426c556a74bcdc8a117771a2e7036fb
Sujal Patel <spatel2@madisoncollege.edu>
2023-03-12 15:30:18 -0500

    Update GitHubBornOn.js

--- src/GitHubBornOn.js
+++ src/GitHubBornOn.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { API } from 'aws-amplify';
 
 
 export const GithubBornOn = () => {
+
+    const [loading,UpdateLoading] = useState(true);
+
+    useEffect(() => {
+        getUser()
+    });
+
+    async function getUser() {
+        UpdateLoading(true);
+        const data = await API.get('githubborn',`'/born'`);
+        console.log(data);
+        UpdateLoading(false);
+    }
+
     return(
         <>
-        <h2></h2>        
+        {!loading &&
+
+        <h2>still loading</h2>        
+        }
         
         
         </>
