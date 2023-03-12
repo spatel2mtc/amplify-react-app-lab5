c461ed1c2d609be0675118d754723195b40971e0
Sujal Patel <spatel2@madisoncollege.edu>
2023-03-11 22:11:20 -0600

    Updating the Client App

--- src/App.js
+++ src/App.js
@@ -8,12 +8,24 @@ import './App.css';
 
 function App() {
   // Create coins variable and set to empty array
-  const [coins, updateCoins] = useState([])
+  const [coins, updateCoins] = useState([]);
+
+ // Create additional state to hold user input for limit and start properties
+const [input, updateInput] = useState({ limit: 5, start: 0 })
+
+// Create a new function to allow users to update the input values
+function updateInputValues(type, value) {
+  updateInput({ ...input, [type]: value })
+}
+
+
+
 
   // Define function to all API
   const fetchCoins = async() =>
    {
-    const data = await API.get('cryptoapi', '/coins')
+    const { limit, start } = input
+    const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`)
     updateCoins(data.coins)
   }
 
@@ -24,6 +36,18 @@ function App() {
 
   return (
     <div className="App">
+<input
+  placeholder="start"
+  onChange={e => updateInputValues('start', e.target.value)}
+/>
+
+<input
+  onChange={e => updateInputValues('limit', e.target.value)}
+  placeholder="limit"
+/>
+
+<button onClick={fetchCoins}>Fetch Coins</button>
+
       {
         coins.map((coin, index) => (
           <div key={index}>
